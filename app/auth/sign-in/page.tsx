ab03eefe70c74032b9761932ae2a20ef962ca5c7
modev <modev.404@gmail.com>
2024-08-10 09:52:25 +0300

    style

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -3,14 +3,14 @@ import { signIn } from "@/auth"
  
 export default function SignIn() {
   return (
-    <main>
+    <main  className="mx-auto container flex justify-center flex-col h-screen items-center space-y-6">
         <form
             action={async () => {
                 "use server"
                 await signIn("google")
             }}
             >
-            <button type="submit">Sign-in with Google</button>
+            <button className="p-2 w-full bg-slate-600 text-white shadow-md  rounded-md" type="submit">Sign-in with Google</button>
         </form>
         <form
             action={async () => {
@@ -18,7 +18,7 @@ export default function SignIn() {
                 await signIn("github")
             }}
             >
-            <button type="submit">Sign-in with Github</button>
+            <button className="p-2 w-full bg-slate-600 text-white shadow-md  rounded-md"  type="submit">Sign-in with Github</button>
         </form>
     </main>
   )
--- app/auth/sign-out/page.tsx
+++ app/auth/sign-out/page.tsx
@@ -3,12 +3,13 @@ import { signOut } from '@/auth'
 const SignOut = () => {
   return (
     <form
+      className="mx-auto container flex justify-center flex-col h-screen items-center space-y-6"
       action={async () => {
         "use server"
         await signOut()
       }}
     >
-      <button type="submit">Sign-Out</button>
+      <button className="py-2 px-10  bg-red-600 text-white shadow-md  rounded-md" type="submit">Sign-Out</button>
     </form>  )
 }
 
